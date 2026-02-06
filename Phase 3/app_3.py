b672136a0c46296465cbf6c37c5f29414bd9a927
Ankit-Pdl <pdlankit2000@gmail.com>
2026-02-06 12:56:14 +0545

    Added login

--- Phase 3/app_3.py
+++ Phase 3/app_3.py
@@ -1,20 +1,20 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("feedback.html")
+    return render_template('feedback.html')
 
 
-
-@app.route("/feedback",methods=["GET","POST"])
+@app.route('/feedback', methods = ['POST','GET'])
 def feedback():
-    if request.method =="POST":
-        name = request.form.get("name")
-        feedback = request.form.get("message")
-        return render_template("thankyou.html",user=name , message=feedback )
-    return render_template("feedback.html")  
+    if request.method =='POST':
+        name = request.form.get('name')
+        feedback = request.form.get('feedback')
+        return render_template('thankyou.html', name=name, message = feedback)
 
-if __name__ == "__main__":
+    return render_template('feedback.html')
+
+if __name__ =='__main__':
     app.run(debug=True)
